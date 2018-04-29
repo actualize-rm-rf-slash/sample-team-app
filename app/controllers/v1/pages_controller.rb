a983bc5baf1587ba277b5ece05070cc912ac0e0a
Vincent Kwok <vkwok22@gmail.com>
2018-04-29 09:42:25 -0500

    added my name to the json

--- app/controllers/v1/pages_controller.rb
+++ app/controllers/v1/pages_controller.rb
@@ -1,5 +1,6 @@
 class V1::PagesController < ApplicationController
   def index
-    render json: {message: "Hello!"}
+    render json: {message: "Hello!",
+      message1: "Hello Vincent!"}
   end
 end
