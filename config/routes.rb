2bacb29baab5eb06bd10f421b856733e82a64844
Peter Jang <peter.x.jang@gmail.com>
2018-04-29 13:36:18 -0500

    Delete unneeded comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :v1 do
     get "/pages" => "pages#index"
     get "/things" => "things#index"
