b3a92b6fb73b35025bda123efc066e8e239af9d8
Peter Jang <peter.x.jang@gmail.com>
2018-04-29 11:37:50 -0500

    Add peters show action

--- app/controllers/v1/peters_controller.rb
+++ app/controllers/v1/peters_controller.rb
@@ -2,4 +2,8 @@ class V1::PetersController < ApplicationController
   def index
     render json: {message: "Helloos"}
   end
+
+  def show
+    render json: {message: "Helloos from show"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :v1 do
     get "/pages" => "pages#index"
-
     get "/things" => "things#index"
-
     get "/articles" => "aricles#index"
-
     get "/peters" => "peters#index"
+    get "/peters/:id" => "peters#show"
   end
 end
